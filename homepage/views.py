a8b01b13dbb41fc2ec6d3fc6a4cd53f137a3da74
sanjay <sanjaypython22@gmail.com>
2025-12-15 10:14:09 +0530

    p

--- homepage/views.py
+++ homepage/views.py
@@ -61,14 +61,11 @@ def patrika(request):
             uploaded_at__date=selected_date
         ).first()
         patrika = Patrika.objects.get(id=latest_pdf.id)
-        print(latest_pdf.id, '=========================1')
         absolute_image_url = request.build_absolute_uri(latest_pdf.featured_image.url)
     else:
         latest_pdf = Patrika.objects.last()
         patrika = Patrika.objects.get(id=latest_pdf.id)
-        print(latest_pdf.id, '=========================2')
         absolute_image_url = request.build_absolute_uri(latest_pdf.featured_image.url)
-
     context = {
         'pdf': latest_pdf,
         'patrika': patrika,
